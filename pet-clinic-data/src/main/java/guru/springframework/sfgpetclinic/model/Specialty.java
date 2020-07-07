2daf7530b6a4ff505edc4a9425d84d1be953bc39
sergio <powerserg19@hotmail.com>
2020-07-07 06:19:14 -0400

    add pet types closes #25 end of lesson 147

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -1,7 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Specialty extends  BaseEntity {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "specialites")
+public class Specialty extends  BaseEntity {
+    @Column(name = "description")
     private String description;
     public String getDescription(){
        return this.description;
