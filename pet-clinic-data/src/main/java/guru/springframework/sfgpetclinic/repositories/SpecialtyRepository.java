356ece272a6676245df558852e665b66e0f40dbc
sergio <powerserg19@hotmail.com>
2020-07-07 21:09:25 -0400

    add pet types closes #27 end of lesson 149

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialtyRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialtyRepository extends CrudRepository<Specialty,Long> {
+
+
+}
